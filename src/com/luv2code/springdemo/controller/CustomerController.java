ff9fd3edfbdc249799eef734e2e7231aed2bfc5a
Angeline Tan <atan009@gmail.com>
2016-10-31 06:34:09 +0800

    Inject CustomerDAO to the controller. Get customer list  from the DAO and add to the model for passing to view

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,15 +1,32 @@
 package com.luv2code.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+  //Need to inject the customer dao
+  //Spring will scan for a component that implements CustomerDAO interface
+  @Autowired 
+  private CustomerDAO customerDAO; 
+  
   @RequestMapping("/list")
   public String listCustomers(Model theModel) {
+    
+    //get customers from the dao
+    List<Customer> theCustomers = customerDAO.getCustomers();
+    
+    //add the customers to the model
+    
     return "list-customers";
   }
   
