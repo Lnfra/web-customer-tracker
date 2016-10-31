d0bf54ec7f4e333ea834df0c0da13383b2b9eb5e
Angeline Tan <atan009@gmail.com>
2016-10-31 08:02:23 +0800

    Modify @RequestMapping annotation at listCUstomers controller mtd to use @GetMapping so as to constrain the controller to respond only to get requests

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
@@ -19,7 +20,7 @@ public class CustomerController {
   @Autowired 
   private CustomerDAO customerDAO; 
   
-  @RequestMapping("/list")
+  @GetMapping("/list")
   public String listCustomers(Model theModel) {
     
     //get customers from the dao
