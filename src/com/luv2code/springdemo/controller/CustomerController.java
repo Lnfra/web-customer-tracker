8c0ac0d3d41ff90225f62109a587e82d015eac2f
Angeline Tan <atan009@gmail.com>
2016-10-31 18:43:02 +0800

    Added controller action, service and DAO to handle the saving of customer

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springdemo.entity.Customer;
@@ -41,6 +43,16 @@ public class CustomerController {
     theModel.addAttribute("customer", theCustomer);
     
     return "customer-form";
+  }
+  
+  @PostMapping("/saveCustomer")
+  public String saveCustomer(@ModelAttribute("customer") Customer theCustomer ){
+    
+    // save the customer using our service
+    customerService.saveCustomer(theCustomer);
+    
+    //after adding customer redirect back to customer list page
+    return "redirect:/customer/list";
   }
   
 }
--- src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -37,6 +37,14 @@ public class CustomerDAOImpl implements CustomerDAO {
     return customers;
   }
 
+  @Override
+  public void saveCustomer(Customer theCustomer) {
+    //get the current hibernate session
+    Session currentSession = sessionFactory.getCurrentSession();
+    //save the customer to the database
+    currentSession.save(theCustomer);
+  }
+
 }
 
 
